bcf7497046b35120016f27c5c3cfddc5e32d2ad5
Zony <yamil.segura.sanchez@ciencias.unam.mx>
2026-09-01 13:28:13 -0600

    arreglar algo con respecto a la variable cantidadElementos

--- Practica1/HashTable.java
+++ Practica1/HashTable.java
@@ -4,52 +4,55 @@ public class HashTable {
 
     private int cantidadElementos;
     private int tamano = 7;
-    private LinkedList<Nodo<?>> [] htabla;
+    private LinkedList<Nodo<?>>[] htabla;
 
     public HashTable(int tamano) {
-	this.tamano = tamano;
-    this.cantidadElementos = 0;
-	
-	this.htabla = (LinkedList<Nodo<?>> []) new Linkedlist<>();
-	
-	for(int i = 0; i < htable.length; i++) {
-	    htabla[i] = new LinkedList<>();
-	}
+        this.tamano = tamano;
+        this.cantidadElementos = 0;
+
+        this.htabla = (LinkedList<Nodo<?>>[]) new Linkedlist<>();
+
+        for (int i = 0; i < htable.length; i++) {
+            htabla[i] = new LinkedList<>();
+        }
     }
 
     public int hashFuncion(int k) {
-	return (k%7);
+        return (k % 7);
     }
 
     public void insertar(Nodo<?> nodo) {
-	if(busqueda(nodo.obtenerLlave()) == false) {
-	    htable[hashFuncion(nodo.obtenerLlave())].add(nodo);
-	}
+        if (busqueda(nodo.obtenerLlave()) == false) {
+            htable[hashFuncion(nodo.obtenerLlave())].add(nodo);
+            cantidadElementos++;
+        }
     }
-	
-    public boolean busqueda(int key){
-	int indice = hashFunction(key); 
-	LinkedList<Nodo<?>> listaBuscar = htable[indice];
-	
-	for(Nodo<V> v : listaBuscar){
-            if(v.obtenerLlave() == key){
+
+    public boolean busqueda(int key) {
+        int indice = hashFunction(key);
+        LinkedList<Nodo<?>> listaBuscar = htable[indice];
+
+        for (Nodo<V> v : listaBuscar) {
+            if (v.obtenerLlave() == key) {
                 return true;
             }
             return false;
-	}    
+        }
     }
-    public boolean eliminar(int key){
+
+    public boolean eliminar(int key) {
         int indice = hashFunction(key);
-        if(busqueda(key)){
+        if (busqueda(key)) {
             LinkedList<Nodo<?>> listaEliminar = htable[indice];
             listaEliminar.remove(key);
+            cantidadElementos--;
             return true;
         }
         return false;
     }
 
-    public double factorCarga(){
+    public double factorCarga() {
         return (double) cantidadElementos / tamano;
     }
-    
+
 }
