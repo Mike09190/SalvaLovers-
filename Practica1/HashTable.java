4b0c7a4a8e0013c8691e636e33f3b9212b96b659
Mike <151068637+Mike09190@users.noreply.github.com>
2026-09-01 13:10:32 -0600

    Busqueda/Eliminar

--- Practica1/HashTable.java
+++ Practica1/HashTable.java
@@ -24,6 +24,25 @@ public class HashTable {
 	    htable[hashFuncion(nodo.obtenerLlave())].add(nodo);
 	}
     }
+        public boolean busqueda(int key){
+       int indice = hashFunction(key); 
+       LinkedList<Nodo<?>> listaBuscar = htable[indice];
 
+       for(Nodo<V> v : listaBuscar){
+            if(v.obtenerLlave == key){
+                return true;
+            }
+            return false;
+       }    
+    }
+    public boolean eliminar(int key){
+        int indice = hashFunction(key);
+        if(busqueda(key)){
+            LinkedList<Nodo<?>> listaEliminar = htable[indice];
+            listaEliminar.remove(key);
+            return true;
+        }
+        return false;
+    }
     
 }
