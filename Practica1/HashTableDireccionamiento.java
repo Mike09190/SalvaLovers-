72140243f28e0bca7ffacd8aa71d17a10441c084
Mike <151068637+Mike09190@users.noreply.github.com>
2026-09-05 18:10:54 -0600

    Clases direccionamiento y Nodo solo para esa
    Se agrego el esqueleto para el direccionamiento abierto, junto con un método auxiliar para insertar elementos.
    La clase nodo que utiliza tiene un atributo nuevo estaEliminado que comienza en false, usarlo a la hora de eliminar y buscar para continuar con la busqueda sin romper el programa.

---
+++ Practica1/HashTableDireccionamiento.java
@@ -0,0 +1,94 @@
+import java.util.*;
+
+public class HashTableDireccionamiento<V>{
+//Creacion de atributos
+private ArrayList<Nodo<V>> htabla;
+private int tamano;
+
+
+    /**
+    * Método constructor de la clase HashTableDireccionamiento
+    * @param tamano tamaño de la tabla hash
+    */
+    public HashTableDireccionamiento(int tamano){
+        this.tamano = tamano;
+        htabla = new ArrayList<>(tamano);
+        for (int i = 0; i < tamano; i++) {
+            htabla.add(null);
+        }
+    }
+
+    /**
+     * Método para calcular el índice de la tabla hash
+     * @param key clave para calcular el índice
+     * @return indice calculado 
+     */
+    public int hashFunction(int key){
+        return (key % tamano);
+    }
+
+    /**
+     * Método para buscar un índice disponible
+     * @param key clave para buscar un índice disponible
+     * @return int índice disponible o -1 en caso de que no haya índices disponibles
+     */
+    private int indiceDisponible(int key){
+        int indice = hashFunction(key);
+        int indiceActual;
+        //Usamos la bandera para guardar la primera posicion eliminada con la bandera "Deleted" en caso de que al final no se encuentre un índice null o igual
+        // devolvemos la primera posición con valor "Deleted" que se encontró o -1 en caso de que no se haya encontrado ninguna posición disponible
+        int bandera = -1;
+
+        for(int i=0; i< tamano; i++){
+           indiceActual = (indice + i) % tamano;
+           NodoDireccionamientoAbierto<V> nodo = htabla.get(indiceActual);
+           if(nodo == null ){
+            if(bandera != -1){
+                return bandera;
+            }
+            return indiceActual;
+           }
+           if(!nodo.estaEliminado() && nodo.obtenerLlave() == key){
+            return indiceActual;
+           }
+           if(nodo.estaEliminado() && bandera == -1){
+            bandera = indiceActual;
+           }
+        }
+
+        return bandera;
+    }
+
+    /**
+     * Método para insertar valores en la tabla hash
+     * @param key clave del valor a insertar
+     * @param value valor a insertar
+     */
+
+    /**
+     * Notas de como implementar el método insertar:
+     * 1. Calcular el índice usarno hashFunction(key)
+     * 2.Recorrer la tabla mediante un bucle while hasta encontrar una casilla disponible
+     * 
+     */
+    public void insertar (int key, V value){
+        
+    }
+
+    /**
+     * Método para buscar un valor en la tabla hash
+     * @param key Clave del valor a buscar
+     * @return valor encontrado o null si no se encuentra
+     */
+
+
+    /**
+     * Método para eliminar un valor de la tabla hash
+     * @param key Clave del valor a eliminar
+     * @return boolean true si se eliminó correctamente, false si no se encontró
+     */
+
+    /**
+     * Método para imprimir la tabla hash  
+     */
+}
