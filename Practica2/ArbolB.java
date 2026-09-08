d9c17b9fb4d3ebb0ab8ec784f8dbc3117161a200
Zony <yamil.segura.sanchez@ciencias.unam.mx>
2026-09-08 10:16:25 -0600

    ArbolB

---
+++ Practica2/ArbolB.java
@@ -0,0 +1,73 @@
+public class ArbolB{
+
+    private Nodo raiz;
+    private int m;
+    private int r;
+    private int q;
+    private int numNiveles;
+
+    /**
+     * Método constructor
+     */
+
+    public ArbolB(){
+        this.raiz = null;
+        this.m = 4;
+        this.r = m-1;
+        this.q = (m/2) - 1;
+        this.numNiveles = 0; 
+    }
+
+    /**
+     * Getters
+     */
+
+    public Nodo getRaiz(){
+        return this.raiz;
+    }
+
+    public int getM(){
+        return this.m;
+    }
+
+    public int getR(){
+        return this.r;
+    }
+
+    public int getQ(){
+        return this.q;
+    }
+
+    public int getNumNiveles(){
+        return this.numNiveles;
+    }
+    /**
+     * Método insertar
+     */
+
+    public void insertar(int llave){
+         //revisar que si sean ints
+        Nodo nuevo = new Nodo();
+
+        //Si no hay raíz
+        if(this.raiz == null){
+            nuevo.setLlave(llave);
+            this.raiz = nuevo;
+        }
+
+        //Si el núm
+        if()
+        
+
+        if(nuevo.equals(this.raiz)){
+            nuevo.setLlave(llave);
+        }
+
+
+        nuevo.setLlave(llave);
+
+    }
+
+
+
+}
