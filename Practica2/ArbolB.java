b0b7fa2648812ddcc40c0d583bf95ab35b7db796
Zony <yamil.segura.sanchez@ciencias.unam.mx>
2026-09-22 00:12:23 -0600

    implementacion de eliminar, metodos de redistribucion y fusionar, ademas de unos auxiliares

--- Practica2/ArbolB.java
+++ Practica2/ArbolB.java
@@ -75,6 +75,7 @@ public class ArbolB{
      * @param Nodo nodo el cual se realiza split
      */
     private void split(Nodo nodo){
+        
         int k3 = nodo.obtenLlave(2);
 
         Nodo padre = nodo.getPadre();
@@ -190,15 +191,18 @@ public class ArbolB{
 
 
     public void eliminar(int llave){
+
         //Verificar si existe un árbol
         if(this.raiz == null){
             return;
         }
         Nodo nodo = buscarNodo(llave); //Devuelve el nodo donde se puede encontrar la llave
 
+        // Si la llave no existe, no modificamos nada.
         if(nodo == null || !nodo.buscaLlave(llave)){
             return;
         }
+
         //Llamamos al método recursivo para eliminar una llave
         eliminarRecursivo(nodo, llave);
     }
@@ -209,35 +213,30 @@ public class ArbolB{
      * @param indice
      */
     private void eliminarRecursivo(Nodo nodo, int llave){
-        //Caso 1: El nodo es una hoja, por lo tanto solo eliminamos la llave
+
+        //Caso 1: La llave esta en una hoja
         if(nodo.esHoja()){
             nodo.borraLlave(llave);
-        }
-        //Case 2: El nodo no es una hoja, por lo que debemos ver si su predecesor o sucedor pueden donar
-        if(!nodo.esHoja()){
-            //Verificamos la carga de los hijos
-            int indice = nodo.getIndiceLlave(llave);
+            
+            // Si la raiz tiene tratamiento especial
+            if(nodo == raiz){
+                repararRaiz();
+                return;
+            }
 
-            Nodo hijoIzquierdo = nodo.obtenerHijoIndice(indice);
-            Nodo hijoDerecho = nodo.obtenerHijoIndice(indice + 1);
-
-            int carga = 1;
-            if(hijoIzquierdo != null && hijoIzquierdo.getNumLlaves() > carga){
-                int predecesor = obtenerPredecesor(nodo, indice);
-                nodo.setLlaveIndice(indice, predecesor);
-                eliminarRecursivo(hijoIzquierdo, predecesor);
+            // Si quedo por debajo del minimo
+            if(nodo.getNumLlaves() < q){
+                repararUnderflow(nodo);
             }
-            else if(hijoDerecho != null && hijoDerecho.getNumLlaves() > carga){
-                int sucesor = obtenerSucesor(nodo, indice);
-                nodo.setLlaveIndice(indice, sucesor);
-                eliminarRecursivo(hijoDerecho, sucesor);
-            }
-            else{
-               //Este método aun no existe xdxd fusionar(nodo, indice);
-                eliminarRecursivo(hijoIzquierdo, llave);
-            }
+            return;
         }
+        
+        // Caso 2: la llave esta en un nodo interno.
+        int indice = nodo.getIndiceLlave(llave);
+        eliminarInterno(nodo, indice);
+        
     }
+
 
     /**
      * Método auxiliar que obtiene el predecesor de un nodo
@@ -271,33 +270,230 @@ public class ArbolB{
 
 
     private void eliminarInterno(Nodo nodo, int indice){
-        Nodo izquierdo = nodo.obtenerHijoIndice(indice);
+        int llave = nodo.obtenLlave(indice);
 
-        // Primero intenramos utilizar el predecesor
-        if(izquierdo.getNumLlaves() > q ){
-            Nodo predecesor = obtenerPredecesor(izquierdo);
-            int llavePredecesora = predecesor.obtenLlave(predecesor.getNumHijos() - 1);
+        Nodo hijoIzquierdo = nodo.obtenerHijoIndice(indice);
+        Nodo hijoDerecho = nodo.obtenerHijoIndice(indice + 1);
 
-            nodo.reemplazar();
+        // Caso 1: intentamos primero con el predecesor
+        if(hijoIzquierdo.getNumLlaves() > q){
+            int predecesor = obtenerPredecesor(nodo, indice);
 
-            eliminarLlaveHoja(predecesor, llavePredecesora);
+            // Sustituimos la llave interna
+            nodo.setLlaveIndice(indice, predecesor);
+
+            Nodo nodoPredecesor = buscarNodoRecursivo(hijoIzquierdo, predecesor);
+            eliminarRecursivo(nodoPredecesor, predecesor);
             return;
         }
-        // si no se puede intentamos con el sucesor 
-        Nodo derecho = nodo.obtenerHijoIndice(indice + 1);
 
-        if(derecho.getNumLlaves() > q){
-            Nodo sucesor = obtenerSucesor(derecho);
-            int llaveSucesor = sucesor.obtenLlave(0);
+        // Caso 2: si el izquierdo no puede intentamos con el sucesor
+        if(hijoDerecho.getNumLlaves() > q){
+            int sucesor = obtenerSucesor(nodo, indice);
 
-            nodo.reemplazar();
+            nodo.setLlaveIndice(indice, sucesor);
 
-            eliminarLlaveHoja(sucesor, llaveSucesor);
+            Nodo nodoSucesor = buscarNodoRecursivo(hijoDerecho, sucesor);
+            eliminarRecursivo(nodoSucesor, sucesor);
+            return;
+
+        }
+
+        // Caso 3: Ninguno tiene una llave de sobra, fusionamos
+        Nodo fusionado = fusionar(nodo, indice);
+
+        // la llave que queremos eliminar ahora se encuentra dentro del nodo fusionado
+        eliminarRecursivo(fusionado, llave);
+
+        // La fusión eliminó una llave del nodo padre,
+        // así que ese padre también puede quedar subocupado.
+        if(nodo == raiz){
+            repararRaiz();
+        }else if(nodo.getNumLlaves() < q){
+            repararUnderflow(nodo);
+        }
+    }
+
+    /**
+     * 
+     */
+    private void repararUnderflow(Nodo nodo){
+        // Caso especial en la raiz
+        if(nodo == raiz){
+            repararRaiz();
             return;
         }
-        // ninguno puede prestar, por lo cual fusionamos
-        fusionar(nodo, indice);
+        
+        // Si ya cumple el minimo no hacemos nada
+        if(nodo.getNumLlaves() >= q){
+            return;
+        }
 
+        Nodo padre = nodo.getPadre();
+        int indiceNodo = padre.getHijos().indexOf(nodo);
+
+        // 1. insertar con el hermano izquierod
+        if(indiceNodo > 0){
+            Nodo hermanoIzquierdo = padre.obtenerHijoIndice(indiceNodo - 1);
+
+            if(hermanoIzquierdo.getNumLlaves() > q){
+                redistribuirDesdeIzquierda(nodo, hermanoIzquierdo, padre, indiceNodo);
+                return;
+            }
+        }
+
+        // 2. Si el izquierdo no pudo intentamos con el derecho
+        if(indiceNodo < padre.getNumHijos()-1){
+            Nodo hermanoDerecho = padre.obtenerHijoIndice(indiceNodo + 1);
+
+            if( hermanoDerecho.getNumLlaves() > q){
+                redistribuirDesdeDerecha(nodo, hermanoDerecho, padre, indiceNodo);
+                return;
+            }
+        }
+
+        // 3. Ninguno puede prestar hay que fusionar
+        if(indiceNodo > 0){
+            fusionar(padre, indiceNodo - 1);
+        } else {
+            fusionar(padre, indiceNodo);
+        }
+
+        // La fusión eliminó una llave del padre, así que revisamos ahora al padre.
+        if(padre == raiz){
+            repararRaiz();
+        }else if(padre.getNumLlaves() < q){
+            repararUnderflow(padre);
+        }
+    }
+
+    /**
+     * 
+     */
+    private void redistribuirDesdeIzquierda(Nodo nodo, Nodo hermanoIzquierdo, Nodo padre, int indiceNodo){
+        int indiceSeparador = indiceNodo -1;
+        int llavePadre = padre.obtenLlave(indiceSeparador);
+
+        int indiceUltimaLlave = hermanoIzquierdo.getNumLlaves() -1;
+        int llaveHermano = hermanoIzquierdo.obtenLlave(indiceUltimaLlave);
+
+        // la llave del padre baja al nodo
+        nodo.setLlave(llavePadre);
+        nodo.ordenar();
+
+        // la ultima llave del hermano sube al padre
+        padre.setLlaveIndice(indiceSeparador, llaveHermano);
+
+        // eliminamos del hermano la llave prestada
+        hermanoIzquierdo.eliminarLlaveIndice(indiceUltimaLlave);
+
+        // si no son hojas tambien debemos mover un hijo
+        if(!hermanoIzquierdo.esHoja()){
+            int indiceHijo = hermanoIzquierdo.getNumHijos() -1;
+
+            Nodo hijoPrestado = hermanoIzquierdo.obtenerHijoIndice(indiceHijo);
+
+            hermanoIzquierdo.eliminarHijoIndice(indiceHijo);
+            nodo.agregarHijoAlInicio(hijoPrestado);
+
+        }
+    }
+
+    /**
+     * 
+     */
+    private void redistribuirDesdeDerecha(Nodo nodo, Nodo hermanoDerecho, Nodo padre, int indiceNodo){
+        int indiceSeparador = indiceNodo;
+        int llavePadre = padre.obtenLlave(indiceSeparador);
+
+        int llaveHermano = hermanoDerecho.obtenLlave(0);
+
+        // la llave del padre baja
+        nodo.setLlave(llavePadre);
+        nodo.ordenar();
+
+        // la primera llave del hermano sube al padre
+        padre.setLlaveIndice(indiceSeparador, llaveHermano);
+        hermanoDerecho.eliminarLlaveIndice(0);
+
+        // si son nodos internos el primer hijo del hermano derecho pasa al final del nodo
+        if(!hermanoDerecho.esHoja()){
+            Nodo hijoPrestado = hermanoDerecho.obtenerHijoIndice(0);
+            hermanoDerecho.eliminarHijoIndice(0);
+
+            nodo.setHijo(hijoPrestado);
+            hijoPrestado.setPadre(nodo);
+        }
+    }
+
+    /**
+     * 
+     */
+    private Nodo fusionar(Nodo padre, int indiceSeparador){
+        Nodo izquierdo = padre.obtenerHijoIndice(indiceSeparador);
+        Nodo derecho = padre.obtenerHijoIndice(indiceSeparador + 1);
+
+        int separador = padre.obtenLlave(indiceSeparador);
+
+        // la llave del padre baja al nodo izquierdo
+        izquierdo.setLlave(separador);
+
+        // pasamos todas las llaves del nodo derecho
+        while(derecho.getNumLlaves() > 0){
+            int llave = derecho.obtenLlave(0);
+
+            izquierdo.setLlave(llave);
+            derecho.eliminarLlaveIndice(0);
+        }
+        izquierdo.ordenar();
+
+        // si existen hijos tambien pasan la nodo fusionado
+        while (derecho.getNumHijos() > 0) {
+            Nodo hijo = derecho.obtenerHijoIndice(0);
+            derecho.eliminarHijoIndice(0);
+
+            izquierdo.setHijo(hijo);
+            hijo.setPadre(izquierdo);
+        }
+
+        // el padre pierde la llave separadora
+        padre.eliminarLlaveIndice(indiceSeparador);
+
+        // y pierde el hijo derecho porque fue absorbido 
+        padre.eliminarHijoIndice(indiceSeparador + 1);
+        
+        return izquierdo;
+    }
+
+    /**
+     * 
+     */
+    private void repararRaiz(){
+        if(raiz == null){
+            return;
+        }
+        // si todavia tiene llaves no hay nada que hacer
+        if(raiz.getNumLlaves() > 0){
+            return;
+        }
+        /**
+         * Raiz con llave pero con un hijo 
+         * ese hijo se convierte en la nueva llave
+         */
+        if(raiz.getNumHijos() == 1){
+            Nodo nuevaRaiz = raiz.obtenerHijoIndice(0);
+            nuevaRaiz.setPadre(null);
+
+            raiz = nuevaRaiz;
+            numNiveles--;
+            return;
+        }
+
+        // raiz sin llaves y sin hijos el arbol queda vacio
+        if(raiz.getNumHijos() == 0){
+            raiz = null;
+            numNiveles = 0;
+        }
     }
 }
 
